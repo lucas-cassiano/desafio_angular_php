e97e3ef1e455cf05e2301b6b16dd698ad1cc133c
lucas cassiano alves <45541119+lucas-cassiano@users.noreply.github.com>
2023-11-19 15:24:50 -0300

    Foi criado o método de cadastro
    Foi feito as validações dos dados de entrada do corpo da requisição.

--- laravel/app/Http/Controllers/Collaborador.php
+++ laravel/app/Http/Controllers/Collaborador.php
@@ -5,11 +5,74 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Collaborador as model;
+use Illuminate\Database\QueryException;
+use Illuminate\Support\Facades\Validator;
 
 class Collaborador extends Controller
 {
-    public function cadastro(){
-        return response()->json([]);
+    public function cadastro(Request $request){
+        try{
+
+            $regras = [
+                'nome' => 'required|string|max:100',
+                'email' => 'required|string|email|max:100',
+                'cpf' => 'required|string|max:15|unique:collaborador,cpf',
+                'celular' => 'string|max:15',
+                'conhecimentos' => 'required|array|min:1|max:3',
+            ];
+
+             $mensagens = [
+                'nome.required' => 'O campo nome é obrigatório.',
+                'nome.string' => 'O campo nome deve ser uma string.',
+                'nome.max' => 'O campo nome deve ter no máximo 100 caracteres.',
+                'email.required' => 'O campo email é obrigatório.',
+                'email.string' => 'O campo email deve ser uma string.',
+                'email.email' => 'O campo email é inválido.',
+                'email.max' => 'O campo email deve ter no máximo 100 caracteres.',
+                'cpf.required' => 'O campo cpf é obrigatório.',
+                'cpf.unique' => 'Já existe um registro com este cpf.',
+                'cpf.string' => 'O campo cpf deve ser uma string.',
+                'cpf.max' => 'O campo cpf deve ter no máximo 15 caracteres.',
+                'celular.string' => 'O campo celular deve ser uma string.',
+                'celular.max' => 'O campo celular deve ter no máximo 15 caracteres.',
+                'conhecimentos.required' => 'O campo conhecimentos é obrigatório.',
+                'conhecimentos.array' => 'O campo conhecimentos deve ser um array.',
+                'conhecimentos.min' => 'O campo conhecimentos deve ter no mínimo 1 item.',
+                'conhecimentos.max' => 'O campo conhecimentos deve ter no máximo 3 itens.',
+            ];
+
+            $validator = Validator::make($request->all(), $regras, $mensagens);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+            $collaborador = [
+                'nome' => $request->input('nome'),
+                'email' => $request->input('email'),
+                'cpf' => $request->input('cpf'),
+                'celular' => $request->input('celular'),
+                'conhecimentos' => implode(',', $request->input('conhecimentos'))
+            ];
+            
+            $resultado = model::create($collaborador);
+
+            if($resultado){
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Successfully created'
+                ]);
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Error creating'
+            ], 400);
+        }catch(QueryException $e){
+            return response()->json([
+                'success' => false,
+                'message' => 'Error creating'
+            ], 400);
+        }
     }
 
     public function collaborators(){
@@ -21,7 +84,7 @@ class Collaborador extends Controller
             $count++;
             array_push($array, [
                 'n' => $count,
-                'nome' => $dados['name'],
+                'nome' => $dados['nome'],
                 'cpf' => $dados['cpf'],
                 'email' => $dados['email'],
                 'celular' => $dados['celular'] == null ? '' : $dados['celular'],
