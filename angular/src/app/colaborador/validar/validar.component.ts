de3ca6bf01e7e1878069275f9194e3ebb1d2de32
lucas cassiano alves <45541119+lucas-cassiano@users.noreply.github.com>
2023-11-19 10:57:48 -0300

    Criação da interface para validar o registro

--- angular/src/app/colaborador/validar/validar.component.ts
+++ angular/src/app/colaborador/validar/validar.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-validar',
   templateUrl: './validar.component.html',
-  styleUrls: ['./validar.component.css']
+  styleUrls: ['./validar.component.css'],
 })
 export class ValidarComponent {
+  handleValidar() {
+    alert('Esta valido');
+  }
 
+  handleNotValidar() {
+    alert('Não esta valido');
+  }
 }
