3de61e9b34f0169200ff2dc61c33432ce56a3f28
lucas cassiano alves <45541119+lucas-cassiano@users.noreply.github.com>
2023-11-19 12:09:54 -0300

    Criação de migration

---
+++ laravel/database/migrations/2023_11_19_142133_create_collaborador_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('collaborador', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('email', 100);
+            $table->string('cpf', 11)->unique();
+            $table->string('celular', 11)->nullable();
+            $table->string('conhecimentos');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('collaborador');
+    }
+};
